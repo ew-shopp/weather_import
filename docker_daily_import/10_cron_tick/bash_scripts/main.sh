80947039c86cf6b30f8df9548dcf5eb9d960a528
Your Name <you@example.com>
2018-07-02 09:38:05 +0200

    Cron tick now working

--- docker_daily_import/10_cron_tick/bash_scripts/main.sh
+++ docker_daily_import/10_cron_tick/bash_scripts/main.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 function gracefulshutdown {
-    echo "Main - Received signal to shut down"
-    echo "Main - Deleting run-file"
-    rm $run_file_name
+    echo "Main - Received signal ... shutting down"
+    exit 0
 }
 
 # arg1: retry_max_count
@@ -11,18 +10,20 @@ function gracefulshutdown {
 # arg3: input directory
 # arg4: work directory
 # arg5: output directory
+# arg6: cron string
 
 retry_max_count=${1}
 code_directory=${2}
 input_directory=${3}
 work_directory=${4}
 output_directory=${5}
+cron_string=${6}
 
-echo "Arguments"
-for i in $*; do 
-   echo $i 
-done
-echo "***" 
+#echo "Arguments"
+#for i in $*; do 
+#   echo "$i" 
+#done
+#echo "***" 
 
 # Date script to start
 job_to_run="${code_directory}/make_date_job.sh"
@@ -37,8 +38,16 @@ if [ -f "$init_script" ]; then
    $init_script
 fi
 
+cron_job="${cron_string} /code/make_date_job.sh ${output_directory}"
+# cron_job="* * * * * /code/make_date_job.sh /out"
+echo "$cron_job" | crontab -
+crontab -l
+service cron start
+
+
+echo "Process idle letting cron job do the work ..."
 while true; do
-    echo "// Sleeping 60 Seconds"
+    #echo "// Sleeping 60 Seconds"
     sleep 60
 done
 
