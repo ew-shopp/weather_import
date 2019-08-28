0daac8dbb782f94134459461957aeb181203b523
Your Name <you@example.com>
2019-08-28 17:41:45 +0200

    Fixed bugs in docker file

--- docker_daily_import/common/make_docker.sh
+++ docker_daily_import/common/make_docker.sh
@@ -5,8 +5,11 @@ read -p "Press any key to start ..."
 
 # Create dir if not present
 mkdir -p common_scripts
+mkdir -p common_py
 # Remove old files if present
 rm common_scripts/*
+rm common_py/*
+
 #Copy new files
 cp ../common/* common_scripts
 
